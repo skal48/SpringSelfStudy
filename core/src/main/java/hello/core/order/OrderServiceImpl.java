f058bf203f9212885c454f2fe99ef2502cfa436e
skal48 <skal48@naver.com>
2023-10-04 00:40:34 +0900

    20231003

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,16 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.member.MemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 이 둘은 인터페이스가 아니라 class에 의존하고 있음
+    private DiscountPolicy discountPolicy; //인터페이스에만 의존하도록
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
